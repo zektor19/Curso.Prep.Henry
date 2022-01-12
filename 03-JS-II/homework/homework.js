67ede24db043fa0d46c5d6e5a39b224272277a09
zektor19 <leoguattini19@gmail.com>
2022-01-12 06:32:33 -0300

    Res Problemas JS2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -188,6 +188,7 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  let i=0;
   do {
     i = i +1
     numero = numero + 5;
